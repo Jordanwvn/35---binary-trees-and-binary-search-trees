f3e7674c970cd5bad83b683eae1e8766197292fe
Jordan Van Ness <jordanwvn@gmail.com>
2018-03-06 14:57:23 -0800

    completed isBalanced and added a findParent function.

--- lab-jordan/bst.js
+++ lab-jordan/bst.js
@@ -13,18 +13,18 @@ class BST{
     this.root = root;
   }
 
-  insert(root=this.root, nodeToInsert) {
+  insert(value, root=this.root) {
     // no root
     if (!root) {
-      this.root = nodeToInsert;
+      this.root = new TreeNode(value);
     // going left
-    } else if (nodeToInsert.value < root.value) {
-      root.left ? this.insert(root.left, nodeToInsert)
-        : root.left = nodeToInsert;
+    } else if (value < root.value) {
+      root.left ? this.insert(value, root.left)
+        : root.left = new TreeNode(value);
     // going right
     } else {
-      !root.right ? this.insert(root.right, nodeToInsert)
-        : root.right = nodeToInsert;
+      !root.right ? this.insert(value, root.right)
+        : root.right = new TreeNode(value);
     }
   }
 
@@ -35,97 +35,68 @@ class BST{
           : null;
   }
 
+  findParent(value, root=this.root, parent=null, childDirection='') {
+    return value === root.value ? {parent: parent, direction: childDirection}
+      : value > root.value ? this.find(value, root.right, root, 'right')
+        : value < root.value ? this.find(value, root.left, root, 'left')
+          : null;
+  }
+
   preOrderTraversal(node=this.root, callback) {
     node ? callback(node)
       : node.left ? this.preOrderTraversal(node.left, callback)
         : node.right ? this.preOrderTraversal(node.right, callback)
+          : null;
   }
 
   inOrderTraversal(node=this.root, callback) {
     node.left ? this.inOrderTraversal(node.left, callback)
       : node ? callback(node)
         : node.right ? this.inOrderTraversal(node.right, callback)
+          : null;
   }
 
   postOrderTraversal(node=this.root, callback) {
     node.left ? this.postOrderTraversal(node.left, callback)
       : node.right ? this.postOrderTraversal(node.right, callback)
         : node ? callback(node)
+          : null;
   }
 
-  // TODO doesn't actually work. Left overall side could still be deeper than right overall side
-  // isBalanced() {
-  //   let balanced = true;
-  //   this.preOrderTraversal(this.root, (currentNode) => {
-  //     let leftDepth, rightDepth;
-  //
-  //     // check left-side depth
-  //     node.left ? (node.left || node.right) ? leftDepth = 2 : leftDepth = 1 : leftDepth = 0;
-  //     // check right-side depth
-  //     node.right ? (node.left || node.right) ? rightDepth = 2 : rightDepth = 1 : rightDepth = 0;
-  //
-  //     if ((leftDepth === 0 && rightDepth === 2) || (leftDepth === 2 && rightDepth === 0)) {
-  //       balanced = false;
-  //     }
-  //   })
-  //   return balanced;
-  // }
 
-  // replaceL(node) {
-  //   if (!node.right.right) {
-  //     let result = node.right;
-  //     node.right = node.right.left;
-  //     return result;
-  //   } else {
-  //     return replaceL(node.right);
-  //   }
-  // }
-  //
-  // replaceR(node) {
-  //   if (!node.left.left) {
-  //     let result = node.left;
-  //     node.left = node.left.right;
-  //     return result;
-  //   } else {
-  //     return replaceL(node.left);
-  //   }
-  // }
+  heightOf(node) {
+    // if you've recursed from a leaf, add 0
+    if (!node) return 0;
+    // otherwise, add 1 and recurse over whichever branch is larger
+    return 1 + Math.max(this.heightOf(node.left), this.heightOf(node.right));
+  }
 
-  // remove(root=this.root, value) {
-  //   let target;
-  //   // if the root IS the value
-  //   if (root === value) {
-  //     target = root.left ? 'root left' : root.right ? 'root right' : 'empty';
-  //   // if the root is NOT the value
-  //   } else {
-  //     target = root.left === value ? 'left' : root.right === value ? 'right' : null;
-  //   }
-  //   // base cases
-  //   switch (target) {
-  //
-  //     case 'root left':
-  //       return this.replaceL(root)
-  //
-  //     case 'left':
-  //       return this.replaceL(root[target])
-  //
-  //     case 'root right':
-  //       return this.replaceR(root)
-  //
-  //     case 'right':
-  //       return this.replaceR(root[target])
-  //
-  //     case 'empty': {
-  //       this.root = null;
-  //       return;
-  //     }
-  //     // recursive case
-  //     default: {
-  //       // recurse left
-  //       if (value < root.value) return root.left ? this.remove(root.left, value) : null;
-  //       // recurse right
-  //       if (value > root.value) return root.right ? this.remove(root.right, value) : null;
-  //     }
-  //   }
-  // }
+  isBalanced(node) {
+    if (!this.root) return true;
+
+    let leftHeight = this.heightOf(node.left);
+    let rightHeight = this.heightOf(node.right);
+
+    if (Math.abs(leftHeight - rightHeight) <= 1 &&
+      isBalanced(node.left) &&
+      isBalanced(node.right)) return true;
+
+    return false;
+  }
+
+  replace(node) {
+    switch (true) {
+      case !node.left && !node.right:
+        delete node;
+      case
+    }
+  }
+
+  remove(value) {
+    if (this.root === value) // DO A THING
+
+    if ()
+  }
+
+
 }
