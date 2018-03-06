a51e8e294967db4b44f51f3a463051c30ada83ee
Victoria Oh <veoh1989@gmail.com>
2018-03-06 15:16:21 -0800

    insert testing done

--- lab-jordan/__test__/bst.test.js
+++ lab-jordan/__test__/bst.test.js
@@ -16,22 +16,25 @@ describe('Bst', function () {
   });
 
   describe('#insert', () => {
-    it('should create a Binary Search Tree and set the root to null', () => {
-      let bst = new BST();
-      bst.insert(10);
-      bst.insert(5);
-      bst.insert(12);
-      expect(bst.root).toBe(10);
+    let bst = new BST();
+    bst.insert(10);
+    bst.insert(5);
+    bst.insert(12);
+    bst.insert(15);
+    it('should have a root value of 10', () => {
+      expect(bst.root.value).toBe(10);
     });
 
-    // it('should create a new Bst instance', () => {
-    //   let bst = new BST();
-    //   expect(bst).toBeInstanceOf(BST);
-    // });
+    it('should have a left value of 5', () => {
+      expect(bst.root.left.value).toBe(5);
+    });
 
-    // it('should create a new Bst instance', () => {
-    //   let bst = new BST();
-    //   expect(bst).toBeInstanceOf(BST);
-    // });
+    it('should have a right value of 12', () => {
+      expect(bst.root.right.value).toBe(12);
+    });
+
+    it('should have a right value of 15', () => {
+      expect(bst.root.right.right.value).toBe(15);
+    });
   });
 });
